8ad75e4385c5b37787f994cb95e5008e7a497ca6
devenock <enockomondi305@gmail.com>
2020-11-20 05:04:50 +0300

    Working on the routes

--- server.js
+++ server.js
@@ -13,8 +13,14 @@ app.use(cors());
 app.use(morgan('tiny'));
 app.use(bodyParser.json());
 
+
 // connect to the database
-mongoose.connect('mongodb://localhost:27017/blog');
+mongoose.connect('mongodb://localhost:27017/blog',{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: true,
+    useCreateIndex: true, 
+});
 
 // create a schema to handle post
 const postSchema = new mongoose.Schema({
@@ -45,6 +51,8 @@ const postService = {
 // create postController to handle the finding and saving posts
 
 const postController = {
+
+    // GET /api/post
     find: async(req, res, next) =>{
         try{
             const posts = await postService.find({...req.query});
@@ -55,6 +63,7 @@ const postController = {
         }
     },
 
+    // POST /api/posts
     save: async(req, res, next) =>{
         try{
             const post = await postService.save(req. body);
@@ -65,3 +74,45 @@ const postController = {
         }
     },
 };
+
+// create express routes
+const router = express.Router();
+
+// get route
+router.get('/posts', postController.find);
+
+// post route
+router.post('/posts', postController.save);
+
+app.use('/api', router);
+
+
+// handle  request when a url path is not found
+const notFound = (req, res, next) =>{
+    res.status(404);
+    res.json({
+        status: 404,
+        error: 'not found'
+    });
+};
+
+// handle an error if occured in the controller
+const handleError = (error, req, res, next) => {
+    console.log(error);
+    res.status(error.status || 500);
+    res.json({
+      message: error.message || "failed: not known error",
+      msg: error.msg,
+      stack: error.stack,
+    });
+  };
+
+  app.use(notFound);
+  app.use(handleError);
+
+  const PORT = process.env.PORT || 3000;
+
+  app.listen(PORT, (err) =>{
+      if(err) console.log(err);
+      else console.log(`Server running on port ${PORT}`);
+  });
